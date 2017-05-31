8347b55af055188846ee40b77bc9f071fac8ba8b
tomoya aikawa <tomoya-aikawa@garage.co.jp>
2017-05-31 14:05:36 +0900

    71% accracy architecture

--- cnn.py
+++ cnn.py
@@ -351,20 +351,14 @@ if __name__ == '__main__':
 
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
     model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
     model.add(Dropout(0.5))
     model.add(Dense(len(classes)))
     model.add(Activation('softmax'))
@@ -373,11 +367,11 @@ if __name__ == '__main__':
                   optimizer='adam',
                   metrics=['accuracy'])
 
-    train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+    # model.compile(loss='kullback_leibler_divergence',
+    #               optimizer='adam',
+    #               metrics=['accuracy'])
+
+    train_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
     test_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
